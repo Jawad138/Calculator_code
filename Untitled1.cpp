238efee0372443b68ef5542422fea9cf9c5eca51
Jawad Ahmad <126562685+Jawad138@users.noreply.github.com>
2023-03-25 22:49:17 +0500

    add cpp file of code
    code of calculator, c++ code to make simple calculator

---
+++ Untitled1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch(op) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2;
+            break;
+        default:
+            // If the operator is not one of the four above, print an error message
+            cout << "Error: Invalid operator";
+            break;
+    }
+
+    return 0;
+}
+
